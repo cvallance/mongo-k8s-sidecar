507b24231095a5f4c55c1a4d925b4ec0f33ec39a
root <root@kubernetes-minion-kpzt.c.kumocare-kubernetes.internal>
2015-08-31 21:00:14 +0000

    Read token from file

--- src/lib/k8s.js
+++ src/lib/k8s.js
@@ -1,12 +1,23 @@
 var Client = require('node-kubernetes-client');
 var config = require('./config');
+fs = require('fs');
+
+var readToken='';
+fs.readFile('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  readToken=data;
+});
 
 var client = new Client({
   host:  config.kubernetesROServiceAddress,
   protocol: 'https',
   version: 'v1',
-  token: 'XYZ'
+  token: readToken
 });
+
+// /var/run/secrets/kubernetes.io/serviceaccount
 
 var getMongoPods = function getPods(done) {
   client.pods.get(function (err, podResult) {
