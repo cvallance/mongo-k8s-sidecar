c404e47a20401b1b529616790d588eac79b8f74d
Andre Wanderley de Souza <ienliven@gmail.com>
2015-12-10 18:26:25 -0200

    Update worker.js
    removed left-behind from specific use-case.

--- src/lib/worker.js
+++ src/lib/worker.js
@@ -218,7 +218,7 @@ var podElection = function(pods) {
   });
 
   //Are we the lucky one?
-  return pods.length >= 3 && pods[0].status.podIP == hostIp;
+  return pods[0].status.podIP == hostIp;
 };
 
 module.exports = {
