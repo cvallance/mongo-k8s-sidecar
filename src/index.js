3653629b001dc5b1fbdc9c4b408e1cb0cbbdfcff
Denis Kramer <d.kramer@soton.ac.uk>
2024-03-07 15:32:40 +0100

    removed redundant code and cleaning

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import worker from './lib/worker.js';
 import logger from './lib/logging.js'
 import mongo from './lib/mongo.js'
 
@@ -11,55 +10,47 @@ import { takeOnWorkerRole } from './roles/worker.js';
 import { submitPeriodicJob } from './queues/worker-queue.js';
 
 
-logger.info('Starting up mongo-k8s-sidecar');
+logger.info('Starting up');
 
 const never = new Promise(() => {}) // wait forever
 
-worker.init(async function(err) {
-  if (err) {
-    logger.error(err, 'Error initializing mongo-k8s-sidecar');
-    return;
-  }
-
-  // Ensure we update our roles
-  await subscribeMemberState( async (member,state) => {
-    try {
-      logger.trace({member,state}, "Received member-state broadcast")
-      const mypodname = process.env.POD_NAME || process.env.HOSTNAME
-      if ( member === mypodname ) {
-         (state === 1) ? await takeOnPrimaryRole() : await leavePrimaryRole()
-         await takeOnWorkerRole() // Every known member should act as worker, 
-                                  // this ensures that new hosts start acting as workers
-      }
-    } catch (err) {
-      logger.error({error: err.message}, 'Error processing member-state-changes')
-    }
-  })
-
+// Ensure we update our roles
+await subscribeMemberState( async (member,state) => {
   try {
-    var {db, close} = mongo.getDb()
-    await mongo.replSetGetStatus(db)
-
-    await takeOnWorkerRole()
-    
-  } catch (err) {
-    if (err.code && err.code == 94) {
-      // The mongoDB replica set is not yet initialised. Lets do this now
-      await initialiseReplicaSet()
+    logger.trace({member,state}, "Received member-state broadcast")
+    const mypodname = process.env.POD_NAME || process.env.HOSTNAME
+    if ( member === mypodname ) {
+        (state === 1) ? await takeOnPrimaryRole() : await leavePrimaryRole()
+        await takeOnWorkerRole() // Every known member should act as worker, 
+                                // this ensures that new hosts start acting as workers
     }
-    if (err.code && err.code == 93) { 
-      // The replica set is invalid - lets become the primary 
-      logger.warn({message: err.message}, 'Invalid replica set.')
-    } else {
-      logger.error(err)
-    }  
-  } finally {
-    if (close) close()
+  } catch (err) {
+    logger.error({error: err.message}, 'Error processing member-state-changes')
   }
+})
 
-  // Setup periodic jobs
-  await submitPeriodicJob('reconcile-state', {}, "*/10 * * * * *")
-  await submitPeriodicJob('update-labels', {}, "*/15 * * * * *" )
-  await never
-  //await worker.workloop()
-});
+try {
+  var {db, close} = mongo.getDb()
+  await mongo.replSetGetStatus(db)
+
+  await takeOnWorkerRole()
+  
+} catch (err) {
+  if (err.code && err.code == 94) {
+    // The mongoDB replica set is not yet initialised. Lets do this now
+    await initialiseReplicaSet()
+  }
+  if (err.code && err.code == 93) { 
+    // The replica set is invalid - lets become the primary 
+    logger.warn({message: err.message}, 'Invalid replica set.')
+  } else {
+    logger.error(err)
+  }  
+} finally {
+  if (close) close()
+}
+
+// Setup periodic jobs
+await submitPeriodicJob('reconcile-state', {}, "*/10 * * * * *")
+await submitPeriodicJob('update-labels', {}, "*/15 * * * * *" )
+await never
